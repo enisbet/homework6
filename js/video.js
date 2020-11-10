ba339300e2edc2341012048cc32f65f3f6214677
enisbet <enisbet@umich.edu>
2020-11-10 12:56:35 -0500

    done?

--- js/video.js
+++ js/video.js
@@ -1,14 +1,65 @@
-var video;
+var video = document.getElementById("myVideo")
 
 window.addEventListener("load", function() {
 	video = document.querySelector("#myVideo");
+	console.log("Starting");
+	console.log(video.volume);
 });
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
+	video.play();
+	let vol = document.querySelector("#volume").innerHTML = (video.volume * 100) + '%'
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video")
+	video.pause();
+});
+
+document.querySelector("#volumeSlider").addEventListener("change", function(){
+	console.log(this);
+	console.log(this.value);
+	let vol = document.querySelector("#volume").innerHTML = this.value + '%'
+
+});
+document.querySelector("#mute").addEventListener("click", function(){
+	if (video.muted){
+		video.muted = false;
+		let volSlider = document.querySelector("#volumeSlider").value = 100;
+		let vol = document.querySelector("#volume").innerHTML = 100 + '%';
+	}
+	else {
+		video.muted = true;
+		let vol = document.querySelector("#volume").innerHTML = 0 + '%';
+		let volSlider = document.querySelector("#volumeSlider").value = 0;
+	}
 	
 });
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate - .1;
+	console.log(video.playbackRate);
+	
+});
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate + .1;
+	console.log(video.playbackRate);
+	
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	video.currentTime = video.currentTime + 5;
+	video.loop = true;
+	console.log(video.currentTime);
+	video.play();
+});
+
+document.querySelector("#old").addEventListener("click", function() {
+	video.classList.add('oldTime')
+});
+document.querySelector("#original").addEventListener("click", function() {
+	video.classList.remove('oldTime')
+});
+
 
