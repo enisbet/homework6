b218225a21067dbdcc1f0dd815a4d823738f5de3
enisbet <enisbet@umich.edu>
2020-11-15 13:40:44 -0500

    changes

--- js/video.js
+++ js/video.js
@@ -25,10 +25,11 @@ document.querySelector("#volumeSlider").addEventListener("change", function(){
 
 });
 document.querySelector("#mute").addEventListener("click", function(){
+	var prev = video.volume;
 	if (video.muted){
 		video.muted = false;
-		let volSlider = document.querySelector("#volumeSlider").value = 100;
-		let vol = document.querySelector("#volume").innerHTML = 100 + '%';
+		let volSlider = document.querySelector("#volumeSlider").value = prev * 100;
+		let vol = document.querySelector("#volume").innerHTML = prev * 100 + '%';
 	}
 	else {
 		video.muted = true;
